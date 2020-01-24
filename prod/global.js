1ab0ce960594f821294f34cf53a793d968311b5e
Mitch Steimel <steimel@iastate.edu>
2020-01-24 14:14:54 -0600

    compile

--- prod/global.js
+++ prod/global.js
@@ -1,1 +1,180 @@
-!function(e){var t={};function r(a){if(t[a])return t[a].exports;var n=t[a]={i:a,l:!1,exports:{}};return e[a].call(n.exports,n,n.exports,r),n.l=!0,n.exports}r.m=e,r.c=t,r.d=function(e,t,a){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:a})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var a=Object.create(null);if(r.r(a),Object.defineProperty(a,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var n in e)r.d(a,n,function(t){return e[t]}.bind(null,n));return a},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,t,r){r(1),e.exports=r(2)},function(e,t){$(".stagger-fade").children().addClass("stagger fade"),$(".stagger-fade-in").children().addClass("stagger fade"),$(".stagger-fade-up").children().addClass("stagger fade"),$(".stagger-fade-up-reverse").children().addClass("stagger fade"),$(".stagger-fade-down").children().addClass("stagger fade"),$(".stagger-fade-down-reverse").children().addClass("stagger fade"),$(".stagger-fade-left").children().addClass("stagger fade"),$(".stagger-fade-right").children().addClass("stagger fade");var r=$(window);function a(e){var t=e.currentTarget,r=t.querySelector(".lib-accordion--content");"false"===t.getAttribute("aria-selected")?(t.classList.add("js-open"),t.setAttribute("aria-selected",!0),r.setAttribute("aria-expanded",!0)):(t.classList.remove("js-open"),t.setAttribute("aria-selected",!1),r.setAttribute("aria-expanded",!1))}document.querySelectorAll(".lib-accordion--item").forEach((function(e){e.addEventListener("click",a)})),r.on("scroll resize",(function(){var e=$(".fade"),t=r.height(),a=r.scrollTop(),n=a+t;$.each(e,(function(){var e=$(this),t=e.outerHeight(),r=e.offset().top;r+t>=a&&r+50<=n&&e.addClass("in-view")})),$(".in-view").css({opacity:"1",transform:"translate(0, 0)",visibility:"visible"})})),r.trigger("scroll")},function(e,t){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/global.js":
+/*!**************************!*\
+  !*** ./src/js/global.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// elements animation
+$(".stagger-fade").children().addClass("stagger fade");
+$(".stagger-fade-in").children().addClass("stagger fade");
+$(".stagger-fade-up").children().addClass("stagger fade");
+$(".stagger-fade-up-reverse").children().addClass("stagger fade");
+$(".stagger-fade-down").children().addClass("stagger fade");
+$(".stagger-fade-down-reverse").children().addClass("stagger fade");
+$(".stagger-fade-left").children().addClass("stagger fade");
+$(".stagger-fade-right").children().addClass("stagger fade");
+var $window = $(window);
+
+function check_if_in_view() {
+  var $fade = $('.fade');
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = window_top_position + window_height;
+  $.each($fade, function () {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = element_top_position + element_height; //check to see if this current container is within viewport
+
+    if (element_bottom_position >= window_top_position && element_top_position + 50 <= window_bottom_position) {
+      $element.addClass('in-view');
+    }
+  });
+  $('.in-view').css({
+    'opacity': '1',
+    'transform': 'translate(0, 0)',
+    'visibility': 'visible'
+  });
+}
+
+var accordionItems = document.querySelectorAll('.lib-accordion--item');
+
+function handleAccordionClick(e) {
+  var item = e.currentTarget;
+  var content = item.querySelector('.lib-accordion--content');
+  var ariaValue = item.getAttribute('aria-selected');
+
+  if (ariaValue === "false") {
+    item.classList.add('js-open');
+    item.setAttribute('aria-selected', true);
+    content.setAttribute('aria-expanded', true);
+  } else {
+    item.classList.remove('js-open');
+    item.setAttribute('aria-selected', false);
+    content.setAttribute('aria-expanded', false);
+  }
+}
+
+accordionItems.forEach(function (item) {
+  item.addEventListener('click', handleAccordionClick);
+});
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+/***/ }),
+
+/***/ "./src/scss/global.scss":
+/*!******************************!*\
+  !*** ./src/scss/global.scss ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!*******************************************************!*\
+  !*** multi ./src/js/global.js ./src/scss/global.scss ***!
+  \*******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /Users/steimel/sites/typhoon/src/js/global.js */"./src/js/global.js");
+module.exports = __webpack_require__(/*! /Users/steimel/sites/typhoon/src/scss/global.scss */"./src/scss/global.scss");
+
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=global.js.map
