ac67ab41a296754a85349653904c9994c5a985e9
Mitch Steimel <steimel@iastate.edu>
2020-03-05 14:01:05 -0600

    broke JS files into modules

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,21 +1,58 @@
-const accordionItems = document.querySelectorAll('.lib-accordion--item');
+import anime from 'animejs/lib/anime.es.js';
 
-function handleAccordionClick(e) {
-  const item = e.currentTarget;
-  const content = item.querySelector('.lib-accordion--content');
-  const ariaValue = item.getAttribute('aria-selected');
+const showItems = document.querySelectorAll('.lib-show--item');
+let showAccordionAnimations = [];
+let hideAccordionAnimations = [];
+
+export function handleAccordionClick(e) {
+  const showItem = e.currentTarget;
+  const button = showItem.querySelector('.lib-show--title');
+  const content = showItem.querySelector('.lib-show--content');
+  const ariaValue = button.getAttribute('aria-selected');
+  const contentNum = content.dataset.box;
+
+  function showAccordion() {
+    showAccordionAnimations[contentNum].play();
+  }
+
+  function hideAccordion() {
+    hideAccordionAnimations[contentNum].play();
+  }
 
   if (ariaValue === 'false') {
-    item.setAttribute('aria-selected', true);
-    item.classList.add('js-open');
+    showItem.classList.add('js-open');
+    button.setAttribute('aria-selected', true);
     content.setAttribute('aria-expanded', true);
+    showAccordion();
   } else {
-    item.setAttribute('aria-selected', false);
-    item.classList.remove('js-open');
+    showItem.classList.remove('js-open');
+    button.setAttribute('aria-selected', false);
     content.setAttribute('aria-expanded', false);
+    hideAccordion();
   }
 }
 
-accordionItems.forEach(item => {
+export const accordionAnimation = showItems.forEach(item => {
+  const contentEl = item.querySelector('.lib-show--content');
+  const contentNum = contentEl.dataset.box;
+  const contentHeight = anime.get(contentEl, 'height', 'px');
+  anime.set(contentEl, { height: 0 });
+
+  showAccordionAnimations[contentNum] = anime({
+    targets: contentEl,
+    height: contentHeight,
+    autoplay: false,
+    easing: 'linear',
+    duration: 100,
+  });
+
+  hideAccordionAnimations[contentNum] = anime({
+    targets: contentEl,
+    height: 0,
+    autoplay: false,
+    easing: 'linear',
+    duration: 500,
+  });
+
   item.addEventListener('click', handleAccordionClick);
 });
